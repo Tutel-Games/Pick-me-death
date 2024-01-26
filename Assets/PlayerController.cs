e936d88f10ab597292622b5e55c2d4a286a75306
5Z3L4 <58202728+5Z3L4@users.noreply.github.com>
2024-01-27 00:40:20 +0100

    xD

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,12 +10,18 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private GameObject _rightAttackSphere;
     [SerializeField] private InputReader _inputs;
     [SerializeField] private SpriteRenderer _sr;
-
+    [SerializeField] private Sprite _idleSprite;
+    [SerializeField] private Sprite _normalAttack1;
+    [SerializeField] private Sprite _normalAttack2;
+    
     private GameObject _currentActiveObj;
-
+    private List<Sprite> _normalAttacks = new();
+    private float _timer;
     private void Start()
     {
         _currentActiveObj = _leftAttackSphere;
+        _normalAttacks.Add(_normalAttack1);
+        _normalAttacks.Add(_normalAttack2);
     }
 
     void Update()
@@ -29,12 +36,29 @@ public class PlayerController : MonoBehaviour
             _sr.flipX = false;
             SetActiveObj(_rightAttackSphere);
         }
+
+        if (_timer > 0)
+        {
+            _timer -= Time.deltaTime;
+        }
+        else
+        {
+            SetAllObjUnactive();
+            _sr.sprite = _idleSprite;
+        }
     }
 
     void SetActiveObj(GameObject newObj)
     {
+        _timer = 0.1f;
         _currentActiveObj.SetActive(false);
         _currentActiveObj = newObj;
         _currentActiveObj.SetActive(true);
+        _sr.sprite = _normalAttack1;
+    }
+
+    void SetAllObjUnactive()
+    {
+        
     }
 }
