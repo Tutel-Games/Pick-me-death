2f5efd0313c633cde40d0d7501973ec211c35b08
5Z3L4 <58202728+5Z3L4@users.noreply.github.com>
2024-01-26 21:35:49 +0100

    init

---
+++ Assets/InputReader.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class InputReader : MonoBehaviour
+{
+    public bool IsJumping;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+        }
+    }
+}
