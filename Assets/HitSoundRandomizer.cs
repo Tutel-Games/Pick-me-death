dd30b3cefea38da8bb7253c73d177340d0cfa4d7
danioper <danio4242@gmail.com>
2024-01-27 20:26:17 +0100

    NewSounds

--- Assets/HitSoundRandomizer.cs
+++ Assets/HitSoundRandomizer.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class HitSoundRandomizer : MonoBehaviour
 {
     [SerializeField] private AudioClip[] _audioClips;
+    [SerializeField] private AudioClip[] _audioClap;
     [SerializeField] private AudioSource _audioSource;
     private int _random;
+    private int _random2;
     [ContextMenu("playaudio")]
     public void PlaySound()
     {
         _random = Random.Range(0, 6);
+        _random2 = Random.Range(0, 4);
+        _audioSource.PlayOneShot(_audioClap[_random2]);
         _audioSource.PlayOneShot(_audioClips[_random]);
     }
 }
