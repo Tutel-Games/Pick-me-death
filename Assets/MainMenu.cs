736790ea6be782fe0fb0f43d422ecf24a9b947f3
Korneliaaaaa <157887681+Korneliaaaaa@users.noreply.github.com>
2024-01-27 18:59:12 +0100

    Main menu 😊

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void PlayGame()
     {
-        
+        SceneManager.LoadSceneAsync("Testando");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public void QuitGame()
+{
+    Application.Quit();
+}
 }
