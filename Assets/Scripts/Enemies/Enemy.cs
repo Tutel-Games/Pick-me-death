2134465e802d2d335d54a2e038e8006ee32522a5
Krystian Kuska <krystiank.2000.kk@gmail.com>
2024-01-27 17:15:24 +0100

    added karol wojtyla papiez polak ojciec swiety and fixes

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -5,16 +5,23 @@ public class Enemy : MonoBehaviour
 {
     public event Action<Enemy> Death;
     public bool MoveRight;
-    [SerializeField] private int _health;
+    [SerializeField] private int _health = 2;
     [SerializeField] private float _movementSpeed;
     [SerializeField] private bool _isDead;
     private Animator _animator;
     private Rigidbody2D _rb;
+    private SpriteRenderer _spriteRenderer;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        _spriteRenderer.flipX = MoveRight;
     }
 
     private void FixedUpdate()
@@ -24,6 +31,15 @@ public class Enemy : MonoBehaviour
         float direction = MoveRight ? 1 : -1;
         Vector2 move = new Vector2(transform.position.x + direction * (_movementSpeed * Time.fixedDeltaTime), transform.position.y);
         _rb.MovePosition(move);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("StopPosition"))
+        {
+            _movementSpeed = 0;
+            //TODO: ATTACK PLAYER
+        }
     }
 
     public void GetDamage()
